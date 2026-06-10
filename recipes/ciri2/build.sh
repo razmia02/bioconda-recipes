a294ddbfb356a57bfba2de6c8abd0f17c60ca4d1
razmia02 <razmia.sabahat@gmail.com>
2026-06-11 01:55:52 +0500

    Added recipe for ciri2 v2.0.6

---
+++ recipes/ciri2/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# Create a binary directory in conda environment 
+
+mkdir -p $PREFIX/bin
+
+# Unzip the download directory
+
+unzip download
+
+# Copy the perl script to binary
+
+cp CIRI_v2.0.6/CIRI2.pl $PREFIX/bin/CIRI2.pl
+
+# Make it executable 
+
+chmod +x $PREFIX/bin/CIRI2.pl
